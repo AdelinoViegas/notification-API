b07a10bab38bc802c31e5246dfd5b0acadc99342
0xff <worktrabalho633@gmail.com>
2024-12-19 22:48:21 +0100

    Manter as configurações padrão

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  /*Estava testando o arquivo pode ser eliminado*/
-  experimental:{
-    serverActions:{
-      bodySizeLimit: "4mb",
-    }
-  }
 };
 
 export default nextConfig;
