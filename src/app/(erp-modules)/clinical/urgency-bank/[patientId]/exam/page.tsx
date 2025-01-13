df183a454fc9b5fd3585bcedcca783d6c3e735b7
0xff <worktrabalho633@gmail.com>
2025-01-13 13:12:15 +0100

    Revisão e ajustess

--- src/app/(erp-modules)/clinical/urgency-bank/[patientId]/exam/page.tsx
+++ src/app/(erp-modules)/clinical/urgency-bank/[patientId]/exam/page.tsx
@@ -1,17 +1,3 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-import Button from "@/components/ui/button";
-import Link from "next/link";
-
 export default function Page(){
-  const path = usePathname();
-  return(
-    <div className="flex gap-x-2">
-      <Link href={`${path}`+"/see-exames"}>
-        <Button>Ver Exames</Button>
-      </Link>
-      <Button className="bg-slate-700">Requisitar Exames</Button>
-    </div>
-  );
+  return;
 }
