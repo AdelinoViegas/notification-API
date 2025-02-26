3828acce1fbdcdcf7ef625e899f3440de2dfa240
0xff <worktrabalho633@gmail.com>
2025-02-26 16:12:32 +0100

    Aprimoramentos

--- src/app/clinical/patient/[patientId]/page.tsx
+++ src/app/clinical/patient/[patientId]/page.tsx
@@ -1,8 +1,53 @@
+import Header from "@/components/header";
+import TabNav from "@/components/tabnav";
+import SendToScreening from "@/components/send-screening";
+import Card from "@/components/ui/card";
 import PatientForm from "@/components/forms/patient-form";
+import ScheduleAppointment from "@/components/forms/schedule-appointment";
+import RequestExams from "@/components/forms/request-exam";
 
-export default async function Page({ params }:{
-	params: Promise<{ patientId: string }>
+type Route = "patient" | "appointment" | "exams";
+
+export default async function Page({ 
+	params,
+	searchParams 
+}:{ 
+	params: Promise<{ patientId: string }>;
+	searchParams: Promise<{ r: Route }>;
 }){
-  const { patientId } = await params;
-	return <PatientForm {...{patientId}} /> 
+	const [{ patientId }, { r }] = await Promise.all([
+		params,
+		searchParams
+	]);
+	
+	return(
+		<div>
+			<div className="mt-4 mb-6">
+				<Header title="Utente Registrado">
+					<SendToScreening />
+				</Header>
+			</div>
+		
+			<Card className="max-h-[80vh]">
+        <TabNav
+          keyParam="" 
+          useReactHook
+					idAsIndexPage
+					baseUrl="/clinical/patient"
+					subPaths={[
+						{ path: "patient", title: "Ficha de Cadastro" },
+						{ path: "appointment", title: "Agendar Consulta" },
+						{ path: "exams", title: "Agendar Exame" },
+						// { path: "card", title: "Cartão do Utente" }
+					]}
+        />
+
+        <div className="max-h-[60vh] overflow-auto px-2 py-3">
+         { r === "patient" && <PatientForm {...{patientId}}/> }
+				 { r === "appointment" &&  <ScheduleAppointment {...{patientId}} /> }
+				 { r === "exams" &&  <RequestExams {...{patientId}} isFullWindow /> }
+        </div>
+      </Card>
+		</div>
+	)
 }
