ff1d3ce30383b889b5121f2bd49611d496d051da
0xff <worktrabalho633@gmail.com>
2025-07-04 22:53:43 +0100

    update: configuração das variaveisde ambiente para o CI/CD

--- src/app/backend/models/con.ts
+++ src/app/backend/models/con.ts
@@ -1,17 +1,7 @@
 import { createConnection } from "mongoose";
 
-const managerDB = process.env.MODE === "container" 
-? "mongodb://internal.containers.host:27017/mw_manager"
-: String(process.env.MASTER_KERNEL)+String(process.env.SUFFIX_DB);
-const clinicalDB = process.env.MODE === "container" 
-? "mongodb://internal.containers.host:27017/mw_clinical"
-: String(process.env.MASTER_CLINICAL)+String(process.env.SUFFIX_DB); 
+const clinical = createConnection(`${process.env.MONGO_URL}/clinical`);
+const kernel = createConnection(`${process.env.MONGO_URL}/manager`);
 
-const clinical = createConnection(clinicalDB);
-const kernel = createConnection(managerDB);
-
-export {
-  kernel,
-  clinical
-}
+export { kernel, clinical }
 
