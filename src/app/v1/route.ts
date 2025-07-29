41ee116d34036f4c0d6e19360f060abf60f395b9
0xff <worktrabalho633@gmail.com>
2025-07-29 11:00:54 +0100

    add: disabilitação do fluxo de autorização

--- src/app/v1/route.ts
+++ src/app/v1/route.ts
@@ -1,18 +1,20 @@
 import { NextResponse as Res, NextRequest as Req } from "next/server";
 import { whoIsUser } from "@/lib/web-token";
-import { checkUserPermission } from "../backend/api/manager/api";
+// import { checkUserPermission } from "../backend/api/manager/api";
 
 export async function GET(req: Req){
   try{
     const userId = await whoIsUser();
+
     if(!userId || !req.nextUrl.searchParams.has('p'))
       throw new Error("sem login iniciado!");
 
-    const pathname = atob(req.nextUrl.searchParams.get('p') as string);
-    const response = await checkUserPermission(pathname);
+    // const pathname = atob(req.nextUrl.searchParams.get('p') as string);
+    // console.log(pathname);
+    // const response = await checkUserPermission(pathname);
 
-    if(!response.status)
-      throw new Error(response.message);
+    // if(!response.status)
+    //   throw new Error(response.message);
     
     return Res.json({ status: true });
   }catch(e){
