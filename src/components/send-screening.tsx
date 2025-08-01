14a6c39a077f988d5ba48cb75db01ef5026665f3
0xff <worktrabalho633@gmail.com>
2025-08-01 12:34:45 +0100

    add: redirecionamento para a pagina de pacientes

--- src/components/send-screening.tsx
+++ src/components/send-screening.tsx
@@ -7,17 +7,20 @@ import {
 import { useParams } from "next/navigation";
 import Button from "@/components/ui/button";
 import { putInScreening } from "@/app/backend/api/clinical/api";
-
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
 export default function SendToScreening(){
   const [ state, action ] = useActionState(putInScreening, { message: "", status: false });
   const urlParams:{ patientId: string } = useParams();
+  const router = useRouter();
   
   useEffect(()=>{
     if(state.message){
       if(state.status)
-        toast.success(state.message, { autoClose: 1500 })
+        toast.success(state.message, {
+          onOpen: ()=> router.replace("/clinical/patient")
+        })
       else
         toast.warn(state.message)
     }
