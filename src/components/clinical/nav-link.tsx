d8aa6b1e0772765df5281a0b6533f6d42f315552
0xff <worktrabalho633@gmail.com>
2025-08-04 17:26:29 +0100

    add: melhoria na navegação

--- src/components/clinical/nav-link.tsx
+++ src/components/clinical/nav-link.tsx
@@ -1,16 +1,17 @@
 'use client';
 
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import clsx from 'clsx';
 import { type Route } from '@/app/backend/api/manager/types';
 import { clinicalIcons } from '@/components/routes';
 
 export default function NavLink({ routes }:{ routes: Route[] }){
   const pathname = usePathname();
+  const router = useRouter();
   const icons = new Map<string, typeof clinicalIcons[number]>();
   clinicalIcons.forEach(e => icons.set(e.route, e));
-
+  
   return(
     <>
       {routes.map((item, index)=>{
@@ -19,17 +20,20 @@ export default function NavLink({ routes }:{ routes: Route[] }){
         const Icon = icons.get(item.route)?.Icon;
 
         return(
-          <Link
+          <button
             key={index}
-            href={item.href}
-            className={clsx("flex rounded-md py-2 px-3 items-center gap-2 border-2 text-sm font-medium hover:bg-blue-100 hover:hover:text-blue-600",
-            {
-              'text-blue-500 font-bold bg-blue-100 border-blue-300': currentRoute
-            }
+            onClick={()=>{
+              router.replace(item.href);
+              router.refresh();
+            }}
+            className={clsx(
+              "flex rounded-md py-2 px-3 items-center gap-2 border-2",
+              "text-sm font-medium hover:bg-blue-100 hover:hover:text-blue-600",
+              { "text-blue-500 font-bold bg-blue-100 border-blue-300": currentRoute }
             )}>
             {!!Icon && <Icon fontSize={25} />}
             <p className="hidden md:block">{item.label}</p>
-          </Link>
+          </button>
         );
       })}
     </>
