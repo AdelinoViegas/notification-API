d9134c83695bed302c31417d6068e1b1596d303f
0xff <worktrabalho633@gmail.com>
2025-08-05 11:41:02 +0100

    update: busca inteligente

--- src/components/clinical/nav-label.tsx
+++ src/components/clinical/nav-label.tsx
@@ -11,8 +11,8 @@ export default function NavLabel({ routes }: { routes: RouteMap }){
   const pathname = usePathname();
 
   return(
-    <div className="px-8 lg:px-48 border-b">
-      <h2 className="text-xl font-medium">{routes.get(pathname)?.label}</h2>
+    <div className="px-8 lg:px-48 border-b py-3">
+      <h2 className="text-xl font-medium">{routes.get(pathname.split("/").slice(0, 3).join("/"))?.label}</h2>
     </div>
   )
 }
