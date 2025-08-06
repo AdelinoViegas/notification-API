dd4fc88d58d7b39971c6991b62103636249ceae2
0xff <worktrabalho633@gmail.com>
2025-08-06 22:21:15 +0100

    fix: correção

--- src/components/forms/screening-ui.tsx
+++ src/components/forms/screening-ui.tsx
@@ -164,7 +164,7 @@ export default function ScreeningUI({
         {ui === "reason" && 
           <>
             <InputDetails
-              textLabel="Descrever o motio da vinda"
+              textLabel="Descrever o motivo da vinda"
               placeholder="Descreva o motivo da vinda do utente..."
               name="reason"
               defaultValue={screeningData?.reason}
