be4b47431173b801715dd12a57376879d0441ab9
0xff <worktrabalho633@gmail.com>
2025-09-02 15:10:20 +0100

    add: endereço url base do serviço de arquivos

--- src/components/user-file-viewer.tsx
+++ src/components/user-file-viewer.tsx
@@ -4,11 +4,12 @@ import UserViewerButton from "@/components/user-viewer-button";
 export default async function UserFileViewer({ id }:{ id: string }){
   try{
     const file = await getFile(id);
+    const origin = new URL(process.env.STORAGE_SRV_URL as string)?.origin;
 
     return (
       <UserViewerButton driveFile={{
         name: file.name,
-        link: [process.env.STORAGE_SRV_URL, file.link].join(""),
+        link: [origin, file.link].join(""),
         size: file.size,
         extension: file.extension
       }} />
