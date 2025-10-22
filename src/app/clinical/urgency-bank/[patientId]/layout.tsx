7ebb6330d13acaf7909724305666747b3b239a45
0xff <worktrabalho633@gmail.com>
2025-10-22 13:08:32 +0100

    add: disposição dos botões

--- src/app/clinical/urgency-bank/[patientId]/layout.tsx
+++ src/app/clinical/urgency-bank/[patientId]/layout.tsx
@@ -5,7 +5,6 @@ import TabNav from "@/components/tabnav";
 import Card from "@/components/ui/card";
 import { MonitorAccess, UnlockProcessAccess } from "@/components/lock-unlock-monitor-process";
 import { getPatient } from "@/backend/api/clinical/urgency-bank-api";
-import RequestConsult from "@/components/request-consult";
 import QuickFabShurtcut from "@/components/quick-fab-shurtcut";
 
 export default async function Layout({ 
@@ -35,9 +34,7 @@ export default async function Layout({
         place="urgency"
         basePathname="/clinical/urgency-bank" 
       />
-
-      <RequestConsult />
-
+      
       <div className={clsx("my-4 text-center pt-3 text-white",
         {"bg-red-500 animate-pulse": patient.screening.priority === "red"},
         {"bg-blue-500": patient.screening.priority === "blue"},
