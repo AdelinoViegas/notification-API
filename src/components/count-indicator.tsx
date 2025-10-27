c3116f7c556b0270d3430c20023edc68cd513153
0xff <worktrabalho633@gmail.com>
2025-10-27 13:34:19 +0100

    update

--- src/components/count-indicator.tsx
+++ src/components/count-indicator.tsx
@@ -7,10 +7,10 @@ export default async function CountIndicator({ from }: { from: CountIndicator })
 
   return(
     <>
-      <div className="relative inline-flex">
+      {quantity > 0 && <div className="relative inline-flex">
         <h2 className={clsx("bg-red-500 text-white inline-flex p-2 text-xs font-bold rounded-full", quantity <= 9 && "px-3")}>{quantity > 50 ? "50": quantity}</h2>
         {quantity > 50 && <p className="absolute top-0 right-0 text-white">+</p>}
-      </div>
+      </div>}
     </>
   )
 }
