1e39ac3b1e0238c22dfd1e15949547dca2dd3468
0xff <worktrabalho633@gmail.com>
2025-10-29 09:53:56 +0100

    add: interfaces globais

--- src/backend/api/clinical/office-api.ts
+++ src/backend/api/clinical/office-api.ts
@@ -473,9 +473,12 @@ async function registerRequest(prev: unknown, formData: FormData){
     }
   }catch(e){
     console.error(e);
+    const err  = e as MongoError
 
     return {
-      message: "Não foi possivel!",
+      message: err.code === 11000 
+      ? "Esta sólicitação ja foi feita!"
+      : "Não foi possivel!",
       status: false
     }
   }
