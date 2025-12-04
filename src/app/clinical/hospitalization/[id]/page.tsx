92d546fa5acded964ba731d46a3db577b5ca78e6
0xff <worktrabalho633@gmail.com>
2025-12-04 15:36:17 +0100

    add: aviza o local de internamento

--- src/app/clinical/hospitalization/[id]/page.tsx
+++ src/app/clinical/hospitalization/[id]/page.tsx
@@ -1,5 +1,6 @@
-import { getTransation, resolvedBed } from "@/backend/api/clinical/hospitalization-api";
+import { getTransation } from "@/backend/api/clinical/hospitalization-api";
 import Accommodate from "@/components/hospitalization/accommodate";
+import Alert from "@/components/ui/alert";
 
 export default async function Page({ params }:{ params: Promise<{ id: string }> }){
   const { id: patientId } = await params;
@@ -7,7 +8,12 @@ export default async function Page({ params }:{ params: Promise<{ id: string }> 
 
   return(
     <div>
-      <div className="w-1/2">
+      <div className="w-1/2 space-y-3">
+        <Alert
+          message={`O medico pediu para internar no serviço de ${transation?.destination.name}`} 
+          type="warn"
+        />
+
         <Accommodate
           serviceId={transation?.destination.id}
         />
