b8a100221c45380d412d642523f79ce7dea36e18
0xff <worktrabalho633@gmail.com>
2025-12-08 13:42:12 +0100

    update: acomoda utente ao serviço de internamento

--- src/components/hospitalization/accommodate.tsx
+++ src/components/hospitalization/accommodate.tsx
@@ -2,9 +2,7 @@
 
 import { useState, useActionState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
-import Modal from "@/components/modal";
 import Button from "@/components/ui/button";
-import InputField from "@/components/ui/input-field";
 import { 
   getNursings, 
   getSections, 
@@ -12,43 +10,19 @@ import {
   signToHospitalize,
   signInternalService
 } from "@/backend/api/clinical/hospitalization-api";
-
 import { toast } from "react-toastify";
 import Selection, { SelectionOption } from "@/components/ui/selection";
-import FallbackComponent from "@/components/fallback-components";
 
-interface FallbackProps {
-  serviceId?: string;
-  sectionId?: string;
-  nursingId?: string;
-  bedId?: string;
-}
-
-export default function Accommodate(props: FallbackProps){
-  const [ state, action ] = useActionState(signToHospitalize, { message: "", status: false }); 
-  const [ serviceState, serviceAction ]= useActionState(signInternalService, { message: "", status: false});
-  const [ modalService, setModalService ] = useState(false);
-  
+export default function Accommodate({ internalServiceId }: { internalServiceId: string }){
+  const [ state, action ] = useActionState(signToHospitalize, { message: "", status: false });   
   const [ sections, setSections ] = useState<SelectionOption[]>([]);
   const [ nursings, setNursings ] = useState<SelectionOption[]>([]);
   const [ beds, setBeds ] = useState<SelectionOption[]>([]);
-  const [ selectedSection, setSelectedSection ] = useState<string>(props?.sectionId as string);
-  const [ selectedNursing, setSelectedNursing ] = useState<string>(props?.nursingId as string);
-
+  const [ selectedSection, setSelectedSection ] = useState<string>();
+  const [ selectedNursing, setSelectedNursing ] = useState<string>();
   const router = useRouter();
   const params = useParams();
 
-  useEffect(()=>{
-    if(serviceState.message)
-      if(serviceState.status)
-        toast.success(serviceState.message, { 
-          onClose: () => setModalService(false),
-        });
-      else
-        toast.error(serviceState.message);
-
-  }, [serviceState]);
-  
   useEffect(()=>{
     if(state.message)
       if(state.status)
@@ -59,7 +33,13 @@ export default function Accommodate(props: FallbackProps){
         toast.error(state.message);
 
     getSections().then(setSections);
-    getNursings({ internalServiceId: props.serviceId }).then(setNursings);
+
+    getNursings({ 
+      internalServiceId,
+      sectionId: selectedSection
+    })
+      .then(setNursings);
+
     getBeds(selectedNursing).then(e => setBeds(e.beds));
 
   }, [state, selectedSection, selectedNursing]);
@@ -68,55 +48,32 @@ export default function Accommodate(props: FallbackProps){
     <div>
       <form action={action}>
         <input type="hidden" name="patientId" value={params.id} />
-        <input type="hidden" name="serviceId" value={props.serviceId} />
+        <input type="hidden" name="serviceId" value={internalServiceId} />
 
-       { sections.length ? 
         <Selection
           label="Ala"
           name="sectionId"
           options={sections} 
           onChange={e => setSelectedSection(e.target.value)}
           required
-          defaultValue={props?.sectionId}
-        />: <FallbackComponent className="my-3" />}
+        />
 
-        { nursings.length ? 
         <Selection
           label="Enfermaria"
           name="nursingId"
           options={nursings} 
           onChange={e => setSelectedNursing(e.target.value)}
           required
-          defaultValue={props?.nursingId}
-        />: <FallbackComponent className="my-3" />}
+        />
 
-        {beds.length || selectedNursing ? 
         <Selection
           label="Nº da Cama"
           name="bedId"
           options={beds} 
           required
-          defaultValue={props?.bedId}
-        />: <FallbackComponent className="my-3" />}
-
+        />
         <Button>Salvar</Button>
       </form>
-
-      <Modal
-        title="Novo serviço de Internamento"
-        open={modalService}
-        onClose={()=>setModalService(false)}
-      >
-        <form action={serviceAction}>
-          <InputField
-            textLabel="Nome"
-            name="name"
-            placeholder="Nome do serviço de internamento"
-            required 
-          />
-          <Button>Salvar</Button>
-        </form>
-      </Modal>
     </div>
   )
 }
