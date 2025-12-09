34f2597ff5d6ee75323f2d9aac348fd0ea9377c5
0xff <worktrabalho633@gmail.com>
2025-12-09 14:54:31 +0100

    add: mostra historicorde pedidos feitos

--- src/app/clinical/hospitalization/hosted/[patientId]/surgery/page.tsx
+++ src/app/clinical/hospitalization/hosted/[patientId]/surgery/page.tsx
@@ -1,27 +1,3 @@
-// //import { getSurgery } from "@/backend/api/clinical/urgency-bank-api";
-// import ScheduleSugery from "@/components/forms/schedule-surgery";
-// /*import Surgery from "@/components/forms/surgery";
-// import SurgeryList from "@/components/surgery-list";
-// import Accordium from "@/components/ui/accordium";*/
-
-// export default async function Page({
-//   params,
-// }: {
-//   params: Promise<{ 
-//     patientId: string;
-//   }>
-// }){
-//   const { patientId } = await params;
-
-//   return(
-//     <div>
-//       {/*<Surgery />*/}
-//         <ScheduleSugery {...{patientId}} />
-//       {/*<SurgeryList items={surgeries} />*/}
-//     </div>
-//   )
-// }
-
 import { getRequests } from "@/backend/api/clinical/office-api"
 import RequestSurgery from "@/components/request-surgery";
 import Tag from "@/components/ui/tag";
