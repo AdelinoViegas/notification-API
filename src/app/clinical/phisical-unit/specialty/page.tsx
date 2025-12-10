4d1758456577436b44f8cbf7a4a3d8ca6df97f7e
0xff <worktrabalho633@gmail.com>
2025-12-10 19:19:35 +0100

    fix: melhoria de ui

--- src/app/clinical/phisical-unit/specialty/page.tsx
+++ src/app/clinical/phisical-unit/specialty/page.tsx
@@ -1,22 +1,12 @@
 import { formater } from "@/lib/table-formater";
-import Header from "@/components/header";
 import Table from "@/components/table";
-import Alert from "@/components/ui/alert";
-import Search from "@/components/ui/search";
 import SpecialtyModal from "@/components/specialty-modal";
 import { getSpecialties } from "@/backend/api/clinical/api";
 
 export const dynamic = "force-dynamic";
 
-export default async function Page({
-  searchParams
-}:{
-  searchParams: Promise<{
-    name: string;
-  }>
-}) {
-  const { name } = await searchParams;
-  const specialtiesData = await getSpecialties( name );
+export default async function Page() {
+  const specialtiesData = await getSpecialties();
   const patientRows = formater(specialtiesData, {
     order:[
       "name",
@@ -29,34 +19,13 @@ export default async function Page({
 
   return (
     <main className="space-y-3">
-
-      <div className="mt-6">
-        <Header title="Unidades Externas"/>
-      </div>
-
       <div className="flex gap-x-2 pb-4">
         <SpecialtyModal />
-      </div>
-
-      <div className="flex lg:flex-row justify-between gap-3 items-center">
-        <Alert 
-          type="info" 
-          message="Faça duplo click sobre a unidade para editar!" 
-        />
-
-        <Search
-          className="flex items-center gap-3"
-          filterKey="name"
-          label="Filtar pelo nome da especialidade"
-          placeholder="Buscar pelo nome..."
-        />
       </div>
   
       <Table
         baseRowLink="/clinical/phisical-unit/specialty"
-        columns={[
-          "Especialidade", 
-        ]} 
+        columns={["Especialidade"]} 
         rows={patientRows}
       />
     </main>
