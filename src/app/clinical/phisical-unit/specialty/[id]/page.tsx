0ce2e7d493a4a741172e528f21b1788b1ad3c033
0xff <worktrabalho633@gmail.com>
2025-12-10 19:23:27 +0100

    update: melhoria

--- src/app/clinical/phisical-unit/specialty/[id]/page.tsx
+++ src/app/clinical/phisical-unit/specialty/[id]/page.tsx
@@ -2,18 +2,14 @@ import Header from "@/components/header";
 import { SpecialtyForm } from "@/components/forms/specialty-form";
 import { getSpecialty } from "@/backend/api/clinical/api";
 
-export default async function Page({ params }: {
-  params: Promise<{
-    specialtyId: string;
-  }>
-}){
-  const { specialtyId } = await params;
-  const specialty = await getSpecialty(specialtyId);
+export default async function Page({ params }: { params: Promise<{ id: string }>}){
+  const { id } = await params;
+  const specialty = await getSpecialty(id);
 
   return(
     <main>
       <div className="mt-6">
-        <Header title="Editar Unidade Externa"/>
+        <Header title="Editar Especialidade"/>
       </div>
       
      <SpecialtyForm data={JSON.stringify(specialty)}/>
--- src/app/clinical/phisical-unit/user/[id]/page.tsx
+++ src/app/clinical/phisical-unit/user/[id]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header";
 import Card from "@/components/ui/card";
 import UserClinicalConfig from "@/components/user-clinical-config";
 import UserWorkplace from "@/components/user-workplace";
@@ -22,9 +21,7 @@ export default async function Page({ params }:{ params: Promise<{ id: string }>}
 
   return (
     <main className="space-y-3">
-      <div className="mt-6">
-        <Header title={`Area de Trabalho | ${user.fullname}`}/>
-      </div>
+      <h2 className="uppercase text-lg font-medium">{user.fullname}</h2>
       
       <Card className="grid lg:grid-cols-2 gap-y-3 gap-x-10">
         <UserClinicalConfig 
