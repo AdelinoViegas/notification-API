721a2813d697450972d67a25009aaefaea8c0917
0xff <worktrabalho633@gmail.com>
2025-12-17 10:38:33 +0100

    add: mostra informacoes da transferencia

--- src/app/clinical/patient/tr/[id]/page.tsx
+++ src/app/clinical/patient/tr/[id]/page.tsx
@@ -1,11 +1,23 @@
 import { getTransferedPatient } from "@/backend/api/clinical/api"
+import Tag from "@/components/ui/tag";
+import { getDataAndHoursFormat } from "@/lib/date-formater";
 
 export default async function Page({ params }: { params: Promise<{ id: string }>}){
   const { id } = await params;
   const transfer = await getTransferedPatient(id);
   return(
     <div>
-      <pre>{JSON.stringify(transfer, null, 2)}</pre>
+      <div>
+        <Tag className="inline-flex">Detalhes da Transferencia</Tag>
+        <h2>Nome: {transfer?.patientName}</h2>
+        
+        <div>
+          Motivo: {transfer?.reason}
+        </div>
+
+        <div>Unidade Externa: {transfer?.externalUnit.name}</div>
+        <div>Data: {getDataAndHoursFormat(new Date(transfer?.createdAt as Date))}</div>
+      </div>
     </div>
   )
 }
