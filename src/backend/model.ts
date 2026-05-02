0ebc0170298d38ad0f785d348793b9a1a6daf108
0xff <worktrabalho633@gmail.com>
2026-05-02 22:50:42 +0100

    update: go back to sefl configuration ip version

--- src/backend/model.ts
+++ src/backend/model.ts
@@ -68,7 +68,7 @@ import dbConfigure from "./db.config";
 export const db = createConnection(process.env.MONGO_URL as string, {
   family: process.env.NODE_ENV === "development" 
     ? 4 
-    : 6,
+    : undefined,
   appName: "app-master-clinical"
 });
 
