58a49740fee1548990059fe75855456adf9f9756
0xff <worktrabalho633@gmail.com>
2026-06-04 15:28:21 +0100

    add: migration to internal fetch and new api storage

--- src/backend/api/storage.ts
+++ src/backend/api/storage.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import type { 
   CidResponse, 
   DefaultResponse, 
@@ -9,34 +9,51 @@ import type {
   ResponseDriveFile 
 } from "@/backend/api/types";
 import { getServiceToken } from "@/lib/web-token";
+import { Fetch } from "@/lib/fetch";
+
+const api = new Fetch({
+  baseUrl: String(process.env.API_URL)+"/st/v2",
+  headers: {
+    Authorization: `Bearer ${await getServiceToken()}`
+  }
+});
 
 const instance = axios.create({ 
-  baseURL: process.env.API_URL + "/st/v1",
+  baseURL: process.env.API_URL,
     headers: {
     Authorization: `Bearer ${await getServiceToken()}`
   }
 });
 
-export async function upload(params: unknown, authorId: string){
-  try{
-    const res = await instance.post<FileResponse>("/uploads", params, {
-      headers: { authorId }
-    });
 
-    return res.data;
-  }catch (e) {
-    const err = (e as AxiosError).response?.data as Error;
-    throw new Error(err.message);
-  }
+export async function upload(params: FormData, authorId: string){
+  // try{
+  //   const res = await instance.post<FileResponse>("/uploads", params, {
+  //     headers: { authorId }
+  //   });
+
+  //   return res.data;
+  // }catch (e) {
+  //   const err = (e as AxiosError).response?.data as Error;
+  //   throw new Error(err.message);
+  // }
+
+  const data = await api.post<FileResponse>("/uploads", params, {
+    headers: { "x-auth-author-id": authorId },
+    params: {}
+  });
+
+  return data;
 }
 
 export async function getAllFiles(){
-  const res = await instance.get<ListAllFiles>("/files");
-  return res.data;
+  // const res = await instance.get<ListAllFiles>("/files");
+  // return res.data;
+  return await api.get<ListAllFiles>("/files") as ListAllFiles;
 }
 
 export async function getFile(id: string){
-  const res = await instance.get<ResponseDriveFile>("/files/file", {
+  const res = await instance.get<ResponseDriveFile>("/files/", {
     params: { id }
   });
 
---
+++ src/lib/fetch.ts
@@ -0,0 +1,117 @@
+interface Instance {
+  baseUrl: string;
+  headers: {
+    [key: string]: string;
+  }
+}
+
+interface MethodConfig {
+  params: {
+    [n: string]: string;
+  },
+  headers: {
+    [n: string]: string;
+  }
+}
+
+type HttpPostBody = { [k: string]: string } | FormData;
+
+export class Fetch {
+  #config: Instance;
+  #baseUrl: URL;
+
+  constructor({ baseUrl, headers }: Instance){
+    this.#config = {
+      baseUrl,
+      headers: {
+        "content-type": "application/json",
+        ...headers
+      }
+    }
+
+    this.#baseUrl = new URL(baseUrl);
+  }
+
+  async get<T>(
+    pathname: string, 
+    config: MethodConfig = { params: {}, headers: {} }
+  ): Promise<T | ArrayBuffer> {
+    this.#baseUrl.pathname = pathname;
+    this.#baseUrl.search = "";
+    
+    for (const k in config.params)
+      this.#baseUrl.searchParams.set(k, config.params[k]);
+      
+    const data = await fetch(this.#baseUrl.toString(), { headers: this.#config.headers });
+    
+    if (this.#config.headers["content-type"] !== "application/json"){
+      return await data.arrayBuffer();
+    }
+
+    this.#log({ headers: JSON.stringify(this.#config.headers)});
+
+    return await data.json() as T;
+  }
+
+  async post<T>(
+    pathname: string, 
+    body: HttpPostBody, 
+    config: MethodConfig = { params: {}, headers: {} }
+  ): Promise<T>{
+    this.#baseUrl.pathname = pathname;
+    this.#baseUrl.search = "";
+    
+    for (const k in config.params)
+      this.#baseUrl.searchParams.set(k, config.params[k]);
+    
+    const data = await fetch(this.#baseUrl.toString(), { 
+      method: "POST",
+      headers: {
+        ...this.#config.headers,
+        ...config.headers
+      },
+      body: body instanceof FormData 
+        ? body
+        : JSON.stringify(body),
+    });
+    
+    this.#log({ headers: JSON.stringify(data.headers)});
+
+    return await data.json() as T;
+  }
+
+  // async postFile<T>(
+  //   pathname: string, 
+  //   body: unknown, 
+  //   config: MethodConfig = { params: {}, headers: {} }
+  // ): Promise<T | unknown>{
+  //   this.#baseUrl.pathname = pathname;
+  //   this.#baseUrl.search = "";
+    
+  //   for (const k in config.params)
+  //     this.#baseUrl.searchParams.set(k, config.params[k]);
+      
+  //   const data = await fetch(this.#baseUrl.toString(), { 
+  //     method: "POST",
+  //     headers: {
+  //       ...this.#config.headers,
+  //       ...config.headers,
+  //     },
+  //   });
+    
+  //   this.#log();
+
+  //   if (this.#config.headers["content-type"] !== "application/json"){
+  //     return await data.arrayBuffer();
+  //   }
+
+  //   return await data.json() as T;
+  // }
+
+  #log({ headers }: { headers: string }){
+    console.log({ 
+      url: this.#baseUrl.toString(),
+      headers: JSON.parse(headers)
+    });
+  }
+}
