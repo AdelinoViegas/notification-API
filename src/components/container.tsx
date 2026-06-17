2981b7cd6b7c926cb1922cd96f69c8618dae1913
0xff <worktrabalho633@gmail.com>
2026-06-17 23:48:25 +0100

    clean: commented code

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,22 +1,3 @@
-// import clsx from "clsx";
-
-// type ContainerProps = { 
-//   children: React.ReactNode;
-//   className?: string; // para adicionar style
-// } & React.HtmlHTMLAttributes<HTMLElement>;
-
-// export default function Container({
-//   children,
-//   className,
-//   ...rest
-// }: ContainerProps){
-//   return(
-//     <main {...rest} className={clsx(className, "mx-6 lg:mx-48")}>
-//       {children}
-//     </main>
-//   )
-// }
-
 import clsx from "clsx";
 
 type ContainerProps = React.ComponentPropsWithoutRef<"main">;
