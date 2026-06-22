c913ea534da30a984bc4144d838f5433967a34dd
AdelinoViegas <buchoteclas@gmail.com>
2026-06-22 22:52:25 +0100

    actualiza o componente transfer-history

--- src/components/transfer-history.tsx
+++ src/components/transfer-history.tsx
@@ -1,24 +1,31 @@
+import { formater } from "@/lib/table-formater";
 import Table from "@/components/table";
 import Alert from "@/components/ui/alert";
 import Search from "@/components/ui/search";
-import { formater } from "@/lib/table-formater";
 import Pagination from "@/components/pagination";
 import Refresh from "@/components/refresh";
+import PeriodFilter from "@/components/discharge-history/period-filter";
 import { getTransferHistories } from "@/backend/api/clinical/urgency-bank-api";
 
 export default async function TransferHistory({ 
   name,
   page,
-  registerNumber
+  processNumber,
+  fromDate,
+  toDate
 }: {
   name?: string;
   page?: number;
-  registerNumber?: number;
+  processNumber?: string,
+  fromDate?: string,
+  toDate?: string,
 }){
   const patients = await getTransferHistories({ 
     page: page?Number(page):1,
     name,
-    registerNumber
+    processNumber,
+    fromDate,
+    toDate
   });
   const rows = formater(patients.patients);
   
@@ -26,35 +33,38 @@ export default async function TransferHistory({
     <main className="space-y-3">
       <Refresh />
 
-      <div className="flex flex-col lg:flex-row justify-between lg:items-center">
-        <Alert 
-          type="info" 
-          message="Faça duplo click sobre o utente para seguir com o atendimento!" 
+      <Alert
+        type="info"
+        message="Duplo clique sobre o registo para visualizar o processo completo em modo bloqueado."
+      />
+
+      <div className="flex flex-wrap items-end gap-3">
+        <PeriodFilter />
+
+        <Search
+          className="flex items-end gap-3"
+          filterKey="processNumber"
+          label="Nº Processo"
+          placeholder="Buscar por nº processo..."
         />
 
         <Search
-          className="flex items-center gap-3"
-          filterKey="registerNumber"
-          label="Filtro por nº processo"
-          placeholder="Buscar pelo nº processo..."
-        />
-
-        <Search
-          className="flex items-center gap-3"
+          className="flex items-end gap-3"
           filterKey="name"
-          label="Filtar por nome"
-          placeholder="Buscar pelo nome do utente..."
+          label="Nome do Paciente"
+          placeholder="Buscar pelo nome..."
         />
       </div>
 
       <Table
         baseRowLink="/clinical/historical"
         columns={[
-          "Nº do processo", 
-          "Nome do paciente",
+          "Nº do Processo",
+          "Data da Tranferência",
+          "Nome do Paciente",
+          "Médico Responsável",
         ]} 
         rows={rows}
-        rowLength={8}
       />
 
       <Pagination
