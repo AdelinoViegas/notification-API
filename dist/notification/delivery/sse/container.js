2c13d0ba118c50a4953ee5d1b68341e0116e1506
AdelinoViegas <buchoteclas@gmail.com>
2026-08-24 22:35:08 +0100

    chore: remove a pasta dist

--- dist/notification/delivery/sse/container.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sseConnectionManager = void 0;
-const manager_1 = require("./manager");
-exports.sseConnectionManager = new manager_1.SSEConnectionManager();
--- dist/notification/persistence/container.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notificationRepository = void 0;
-const db_1 = require("./db");
-exports.notificationRepository = new db_1.PrismaNotificationRepository();
