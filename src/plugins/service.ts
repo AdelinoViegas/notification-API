9df9d32bb584692efa72fd1dda50ca9f046d94fb
AdelinoViegas <buchoteclas@gmail.com>
2026-09-21 19:42:14 +0100

    refactor: add serviços no fastify

--- src/plugins/service.ts
+++ src/plugins/service.ts
@@ -1,25 +1,7 @@
 import fp from "fastify-plugin";
-import { prisma } from "../lib/prisma.js";
-import { NotificationService } from "../services/index.js";
+import { notificationService } from "../services/provider/containers";
 
-import { NotificationDispatcher } from "../notification/notificationDispatcher.js";
-import { SSEAdapter } from "../notification/delivery/sse/adapter.js";
-import { SSEConnectionManager } from "../notification/delivery/sse/manager.js";
-
-const sseConnectionManager = new SSEConnectionManager();
-
-const notificationDispatcher = new NotificationDispatcher(
-  new Map([
-    ["sse", new SSEAdapter(sseConnectionManager)]
-  ])
-);
-
-const services = {
-  notificationService: new NotificationService({
-    database: prisma,
-    notificationDispatcher,
-  }),
-}
+const services = { notificationService }
 
 export default fp(
   async function (fastify) {
